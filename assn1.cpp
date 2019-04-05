b98cc4d3db2f2e5e0d94d749d378c0746dbeebd5
Zeandré Lindsey <lyricist824@gmail.com>
2019-04-05 13:08:04 +0000

    Still displays wireframe. Need to adjust file parsing and work on options.

--- assn1.cpp
+++ assn1.cpp
@@ -34,12 +34,17 @@ typedef struct{
 Color *colorA, *colorB;			//colorA points to the starting color and colorB points to the end color
 Line line;
 Curve curve;
+const int numOfPoints = 15;
+float points[numOfPoints][2];
+const int numOfFaces = 2 * 15;
+int faces[numOfFaces][3];
 
 //These variables are used in conjuction with the keyboard inputs
 //to permit certain functionality depending on the input
 bool linesActive = false;
 bool curvesActive =  false;
 bool erase = false;
+bool drawn = false;
 
 int x_last,y_last;
 
@@ -73,16 +78,16 @@ void dda(){		//Function implementing the DDA algorithm
   float slope = line.getSlope();
   
   //This chunk helps in incrementing from the first color to the second color
-  double rDiff = colorB->r - colorA->r;			//This calculates the difference between the red values
-  double gDiff = colorB->g - colorA->g;			//This calculates the difference between the green values
-  double bDiff = colorB->b - colorA->b;			//This calculates the difference between the blue values
+  double rDiff = colorA->r;			//This calculates the difference between the red values
+  double gDiff = colorA->g;			//This calculates the difference between the green values
+  double bDiff = colorA->b;			//This calculates the difference between the blue values
   
   //Chooses this if the |slope| is less than 1 and increments along the x-values
   if (fabs(slope) < 1){
   	int xDiff = line.getPoint(1).x-line.getPoint(0).x;
   	int xInc = 0;
     while (line.haveAllPoints() && x != line.getPoint(1).x){
-      write_pixel((int)x, (int)round(y),(rDiff/xDiff)*xInc,(gDiff/xDiff)*xInc,(bDiff/xDiff)*xInc);
+      write_pixel((int)x, (int)round(y),(rDiff),(gDiff),(bDiff));
       (line.getXdiff() < 0) ? x-- : x++;
       (line.getYdiff() < 0) ? y-=fabs(slope) : y+=fabs(slope);
       xInc++;
@@ -94,7 +99,7 @@ void dda(){		//Function implementing the DDA algorithm
     int yDiff = line.getPoint(1).y-line.getPoint(0).y;
     int yInc = 0;
     while (line.haveAllPoints() && y != line.getPoint(1).y){
-      write_pixel((int)round(x), (int)y,(rDiff/yDiff)*yInc,(gDiff/yDiff)*yInc,(bDiff/yDiff)*yInc);
+      write_pixel((int)round(x), (int)y,(rDiff),(gDiff),(bDiff));
       (line.getXdiff() < 0) ? x-=fabs(slope) : x+=fabs(slope);
       (line.getYdiff() < 0) ? y-- : y++;
       yInc++;
@@ -114,63 +119,55 @@ void display ( void )   // Create The Display Function
     erase = false;
   }
   //This is used to still draw point clicks
-  colorA = new Color{0,0,0};
+/*  colorA = new Color{0,0,0};
   write_pixel(x_last,y_last,1.0,1.0,1.0);//<-you can get rid of this call if you like
-  delete colorA;
-  // CALL YOUR CODE HERE
+  delete colorA;*/
+  
   double r,g,b;
-  int colorDivisions = 100;			//Used to generate color value percentage
-  //Generates random color as colorA (the starting color)
-  r = (rand() % colorDivisions +1)/(double)colorDivisions;
-  g = (rand() % colorDivisions +1)/(double)colorDivisions;
-  b = (rand() % colorDivisions +1)/(double)colorDivisions;
-  colorA = new Color{r,g,b};
-  
-  //Generates random color as colorB (the end color)
-  r = (rand() % colorDivisions +1)/(double)colorDivisions;
-  g = (rand() % colorDivisions +1)/(double)colorDivisions;
-  b = (rand() % colorDivisions +1)/(double)colorDivisions;
-  colorB = new Color{r,g,b};
-  
-  if (linesActive && line.haveAllPoints()) {
-    dda();
-    line.clearPoints();
-    printf("\n");
-	}
-  if (curvesActive && curve.haveAllPoints()){
-    float divisions = 25;			//Represents the number of segments the curve is split into
-		  
-		//This helps in incrementing from the first color to the second color for the purpose of splitting it up over each line segments
-		double rDiff = colorB->r - colorA->r;			//This calculates the difference between the red values
-		double gDiff = colorB->g - colorA->g;			//This calculates the difference between the green values
-		double bDiff = colorB->b - colorA->b;			//This calculates the difference between the blue values
+  if (!drawn){
+	  for (int n = 0; n < numOfFaces; n++){
+		if (!(n%2)){
+			int colorDivisions = 100;			//Used to generate color value percentage
+			//Generates random color as colorA (the starting color)
+			r = (rand() % colorDivisions +1)/(double)colorDivisions;
+			g = (rand() % colorDivisions +1)/(double)colorDivisions;
+			b = (rand() % colorDivisions +1)/(double)colorDivisions;
 		
-		Color startColor = {colorA->r,colorA->g,colorA->b};			//startColor used as the initial value of colorA
-		
-    for(int n = 1; n <= divisions; n++){
-			
-			float u = (n-1)/divisions;		//Calculates u-value based off the percentage for the line
-      Point firstPoint = curve.getPoint(u);			//Point at given u-value
-      line.addPoint(firstPoint.x, firstPoint.y);
-      //Sets colorA (starting color) for the line segment using the u-value 
-      colorA = new Color{startColor.r + (rDiff * u), startColor.g + (gDiff * u), startColor.b + (bDiff * u)};
-     
-      u = (n)/divisions;			//Calculates u-value based off the percentage for the line
-      Point secondPoint = curve.getPoint(u);		//Point at given u-value
-      line.addPoint(secondPoint.x, secondPoint.y);
-      //Sets colorB (end color) for the line segment using the u-value
-      colorB = new Color{startColor.r + (rDiff * u), startColor.g + (gDiff * u), startColor.b + (bDiff * u)};
-     
-      dda();
-      line.clearPoints();
-    	delete colorA;
-    	delete colorB;
-    }
-    curve.clearPoints();
-    printf("\n");
-  }
-  delete colorA;
-  delete colorB;
+		}	
+		colorA = new Color{r,g,b};
+	//	write_pixel(points[n][0],points[n][1],1.0,1.0,1.0);//<-you can get rid of this call if you like
+		int point1X = points[faces[n][0]][0];
+		int point1Y = points[faces[n][0]][1];
+		int point2X = points[faces[n][1]][0];
+		int point2Y = points[faces[n][1]][1];
+		int point3X = points[faces[n][2]][0];
+		int point3Y = points[faces[n][2]][1];
+		line.addPoint(point1X,point1Y);
+		line.addPoint(point2X,point2Y);
+		if (line.haveAllPoints()) {
+			dda();
+			line.clearPoints();
+			printf("\n");
+		}
+		line.addPoint(point2X,point2Y);
+		line.addPoint(point3X,point3Y);
+		if (line.haveAllPoints()) {
+			dda();
+			line.clearPoints();
+			printf("\n");
+		}
+		line.addPoint(point3X,point3Y);
+		line.addPoint(point1X,point1Y);
+		if (line.haveAllPoints()) {
+			dda();
+			line.clearPoints();
+			printf("\n");
+		}
+		delete colorA;
+	  }
+	  drawn = true;
+	}  
+ 
   colorA = nullptr;
   colorB = nullptr;
   glutSwapBuffers();                                      // Draw Frame Buffer
@@ -246,12 +243,50 @@ int main (int argc, char *argv[])
 
 	 FILE *fptr;
 
-	if ((fptr = fopen(argv[1])) == NULL){
+	if ((fptr = fopen(argv[1], "r")) == NULL){
 		printf("Error! opening file");
 
 		// Program exits if the file pointer returns NULL.
 		exit(1);
 	}
+
+	char type;
+	int count = 0;
+	float fileX,fileY;
+	while (fscanf(fptr, "%c %f %f", &type, &fileX, &fileY) != EOF){
+		
+		if (type == 'v'){
+			points[count][0] = fileX;
+			points[count][1] = fileY;
+			count++;
+		}
+	}
+	rewind(fptr);
+/*	char set1[6];
+	char set2[6];
+	char set3[6];*/
+	count = 0;
+	type = ' ';
+	int v1, v2, v3;
+	int vt1, vt2, vt3;
+	int vn1, vn2, vn3;
+	
+	int line = 0;
+	while (fscanf(fptr, "%c %d/%d/%d %d/%d/%d %d/%d/%d \n", &type, &v1, &vt1, &vn1, &v2, &vt2, &vn2, &v3, &vt3, &vn3) != EOF){
+		printf("%d\n", line);
+		if (type == 'f'){
+			printf("%c %d/%d/%d %d/%d/%d %d/%d/%d\n", type, v1, vt1, vn1, v2, vt2, vn2, v3, vt3, vn3);
+			faces[count][0] = v1-1;
+			faces[count][1] = v2-1;
+			faces[count][2] = v3-1;
+			count++;
+		}
+		else{
+			getline(NULL,0,fptr);
+		}
+		line++;
+	}
+	
 
 	srand(time(NULL));
 	glutInit            ( &argc, argv );
